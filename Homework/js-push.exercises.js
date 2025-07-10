064041a9ea53af52c97ab81104ec9f2bca645e5c
EeveSomalia <budiwijaya555@gmail.com>
2025-07-10 16:58:54 +0700

    some fix

--- Homework/js-push.exercises.js
+++ Homework/js-push.exercises.js
@@ -1,6 +1,6 @@
 // add onto end of array next number
-const arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5]; // This will print: [1, 2, 3, 4, 5]
 
 //expected output: [1, 2, 3, 4, 5, 6]
-arr.push(6);
-console.log(arr);
+arr.push(6); // adds 6 to the end of the array
+console.log(arr); // This will print: [1, 2, 4, 5]
