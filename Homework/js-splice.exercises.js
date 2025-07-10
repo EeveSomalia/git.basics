2060941f04b4d75549b235ca4e7e30757fa7a1de
EeveSomalia <budiwijaya555@gmail.com>
2025-07-10 16:54:11 +0700

    doein with splice

---
+++ Homework/js-splice.exercises.js
@@ -0,0 +1,6 @@
+// remove 2 from arr
+const arr = [1, 2, 3, 4, 5]; // This will print: [1, 2, 3, 4, 5]
+
+// expected: [1, 3, 4, 5]
+arr.splice(1, 1); // removes the second element
+console.log(arr); // This will print: [1, 3, 4, 5]
